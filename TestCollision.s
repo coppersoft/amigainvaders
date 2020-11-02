6162a5f80691d21755b9fea62d761fb9b147b0f0
Lorenzo Di Gaetano <lorenzo.digaetano@istat.it>
2020-11-02 18:57:30 +0100

    Test routine collisione

---
+++ TestCollision.s
@@ -0,0 +1,44 @@
+; d0:	srcX
+; d1:	srcY
+
+; d2:	dstX
+; d3:	dstY
+
+; d4:	src box width/2  + dst box width/2
+; d5:	src box height/w + dst box height/2
+
+
+; Prova routine di boundary collision
+
+
+
+	move.w	#100,d0		; srcX
+	move.w	#100,d1		; srxY
+
+	move.w	#100,d2		; dstX
+	move.w	#100,d3		; dstY
+
+	move.w	#(16/2)+(16/2),d4
+	move.w	#(16/2)+(16/2),d5
+
+
+
+Collision:
+	sub.w	d2,d0
+	bpl.s	.nondx
+	neg.w	d0
+.nondx
+	cmp.w	d4,d0
+	bhi.s	.nohitx
+
+	sub.w	d3,d1
+	bpl.s	.nondy
+	neg.w	d1
+
+.nondy
+	cmp.w	d5,d1
+.nohitx
+	rts
+
+
+
