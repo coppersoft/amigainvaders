f72da5b5e36dfa931c93c64f504e078815cfc39e
Lorenzo Di Gaetano <lorenzo.digaetano@istat.it>
2020-12-29 19:19:01 +0100

    credits

--- space.s
+++ space.s
@@ -7,6 +7,8 @@
 
 ;    Copyright (C) 2020 - Lorenzo Di Gaetano <lorenzodigaetano@yahoo.it>
 
+;    Uses P61 mod play routine by Jarno Paananen
+
 ;    This program is free software: you can redistribute it and/or modify
 ;    it under the terms of the GNU General Public License as published by
 ;    the Free Software Foundation, either version 3 of the License, or
