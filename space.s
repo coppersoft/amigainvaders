6b86f20d64c69d05cab89abe9eb463c582547e4f
Lorenzo Di Gaetano <lorenzo.digaetano@istat.it>
2020-12-29 20:02:43 +0100

    Aggiunte istruzioni di compilazione

--- space.s
+++ space.s
@@ -9,6 +9,9 @@
 
 ;    Uses P61 mod play routine by Jarno Paananen
 
+;    Compile with
+;    vasmm68k_mot -Fhunkexe -kick1hunks space.s
+
 ;    This program is free software: you can redistribute it and/or modify
 ;    it under the terms of the GNU General Public License as published by
 ;    the Free Software Foundation, either version 3 of the License, or
